713eca70677488fd915ae7c784e41bb4d7f90b3a
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-05-20 23:45:10 -0700

    add questions for quiz

---
+++ assets/js/js_questions.js
@@ -0,0 +1,84 @@
+let questions = [
+    { 
+        num : 1,
+        question: "Inside which HTML element do we put the JavaScript?", 
+        answer: 0, 
+        options: ["<script>" ,"<js>","<scripting>","<javascript>"]
+    },
+    { 
+        num : 2,
+        question:"Where is the correct place to insert a JavaScript?", 
+        answer: 2, 
+        options:["The <body> section ","The <head> section ","Both the <head> section and the <body> section are correct"]
+    },
+    { 
+        num : 3,
+        question:"How do you create a function in JavaScript?" , 
+        answer:0 , 
+        options: ["function myFunction() ","function = myFunction()","function:myFunction()"]
+    },
+    { 
+        num : 4,
+        question: "How do you round the number 7.25, to the nearest integer?", 
+        answer:1,
+        options: ["round(7.25)","Math.round(7.25)  ","rnd(7.25)","Math.rnd(7.25"]
+    },
+    { 
+        num : 5,
+        question: "Is JavaScript case-sensitive?", 
+        answer: 0, 
+        options: ["Yes ","No"]
+    },
+    { 
+        num : 6,
+        question:"Which sign does jQuery use as a shortcut for jQuery?" , 
+        answer: 0, 
+        options: ["the $ sign  ","the ? Sign","the % sign"]
+    },
+    { 
+        num : 7,
+        question: "Is jQuery a library for client scripting or server scripting?", 
+        answer: 0, 
+        options: ["Client scripting  ","Server scripting"]
+    },
+    { 
+        num : 8,
+        question: "Which jQuery method is used to hide selected elements?", 
+        answer: 2, 
+        options: ["display(none)","visible(false)","hide()","hidden()"]
+    },
+    { 
+        num : 9,
+        question: "What scripting language is jQuery written in?", 
+        answer: 2, 
+        options: ["VBScript","C++","JavaScript ","C#"]
+    },
+    { 
+        num : 10,
+        question:"Which jQuery method returns the direct parent element of the selected element?" , 
+        answer:"parent() " , options: ["ancestors()","ancestor()","parents()","parent() "]
+    },
+    { 
+        num : 11,
+        question: "Which event occurs when the user clicks on an HTML element?", 
+        answer: 0, 
+        options: ["onclick  ","onchange","onmouseover","onmouseclick"]
+    },
+    { 
+        num : 12,
+        question: "How do you declare a JavaScript variable?", 
+        answer: 0, 
+        options: ["v carName;","var carName;  ","variable carName;"]
+    },
+    { 
+        num : 13,
+        question: "Look at the following selector: $(':disabled'). What does it select?", 
+        answer: 1, 
+        options: ["All elements that does not contain the text 'disabled'  ","All disabled input elements  ","All hidden elements","All elements containing the text 'disable'"]
+    },
+    { 
+        num : 14,
+        question: "Which jQuery method is used to switch between adding/removing one or more classes (for CSS) from selected elements?", 
+        answer: 0, 
+        options: ["toggleClass() ","altClass()","switch()","switchClass()"]
+    }];
